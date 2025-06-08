3a4acf4629b97a22f2e3268c44cd3709855e875f
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2025-06-07 22:35:31 -0300

    Configuração inicial com ajustes de css e  atualização da versões do tailwind para @3.4.17

---
+++ psi-wu/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/layout.tsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/app/page.tsx"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "rgb(var(--background))",
+        foreground: "rgb(var(--foreground))",
+      },
+    },
+  },
+  plugins: [],
+}
